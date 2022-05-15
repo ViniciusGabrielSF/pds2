d9821fe20eb161aceb715f7774702ed8f612983a
Vinícius Ferreira <vinicius.ferreira@dtidigital.com.br>
2022-05-15 19:49:01 -0300

    [pratica4] inicia complexos por coordenadas euclidianas

--- pratica4/complexo.cpp
+++ pratica4/complexo.cpp
@@ -5,38 +5,43 @@
 #include <cmath>
 
 Complexo::Complexo() {
+    this->real_ = 0;
+    this->imag_ = 0;
 }
 
 Complexo::Complexo(double a, double b) {
-
+    this->real_ = a;
+    this->imag_ = b;
 }
 
 double Complexo::real() {
-  return 0.0;
+  return this->real_;
 }
 
 double Complexo::imag() {
-  return 0.0;
+  return this->imag_;
 }
 
 bool Complexo::igual(Complexo x) {
-  return 0.0;
+  return this->real_ == x.real() && this->imag_ == x.imag();
 }
 
 void Complexo::Atribuir(Complexo x) {
+    this->real_ = x.real();
+    this->imag_ = x.imag();
 }
 
 double Complexo::modulo() {
-  return 0;
+  return sqrt( pow(this->real_,2) + pow(this->imag_,2));
 }
 
 Complexo Complexo::conjugado() {
-  Complexo c;
+  Complexo c new Complexo{this->real_, (-1 * this->imag_)};
   return c;
 }
 
 Complexo Complexo::simetrico() {
-  Complexo c;
+  Complexo c new Complexo{ ( -1 * this->real_), (-1 * this->imag_)};
   return c;
 }
 
