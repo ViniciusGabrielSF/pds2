f7c689a9908752540304a9e257c387f80f9d8385
root <root@DESKTOP-EJ47PRO.localdomain>
2022-05-15 21:09:27 -0300

    [pratica5] implementa vetor

--- pratica5/vetor.cpp
+++ pratica5/vetor.cpp
@@ -1,6 +1,8 @@
 #include "vetor.h"
 #include <string>
 
+using namespace std;
+
 Vetor::Vetor (int inicio, int fim) {
     this->inicio_ = inicio;
 
@@ -9,12 +11,16 @@ Vetor::Vetor (int inicio, int fim) {
     this-> elementos_ = new string[tamanho];
 }
 
-void Vetor::atribuir(int i, std::string valor){
-    
+void Vetor::atribuir(int i, string valor){
+    int indice = i - this->inicio_;
+
+    this->elementos_[indice] = valor;
 }
 
 string Vetor::valor(int i) const {
+    int indice = i - this->inicio_;
 
+    return this->elementos_[indice];
 } 
 
 Vetor::~Vetor(){
